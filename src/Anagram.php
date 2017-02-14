0521a0809604e7986cdfb72701c21be035921963
Erica Wright and Jason Brown <jason@>
2017-02-14 09:06:59 -0800

    Meet second spec - show if word is anagram

--- src/Anagram.php
+++ src/Anagram.php
@@ -5,8 +5,10 @@
         {
             $first_word_letter_array = str_split($input[0]);
             $word_to_compare_array = str_split($input[1]);
-            if (array_diff($first_word_letter_array, $word_to_compare_array) != 0) {
+            if (array_diff($first_word_letter_array, $word_to_compare_array) != array()) {
                 return "This is not an anagram";
+            } else {
+                return "This is an anagram";
             }
         }
     }
--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -17,7 +17,22 @@
             $this->assertEquals("This is not an anagram", $result);
 
         }
+
+        function test_anagram_yes()
+        {
+            //Arrange
+            $test_word = new Anagram;
+            $input = array("dear", "read");
+            //Act
+
+            $result = $test_word->compareWords($input);
+
+            //Assert
+            $this->assertEquals("This is an anagram", $result);
+
+        }
     }
+
 
 
 
